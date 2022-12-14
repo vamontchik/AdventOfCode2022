881811af182db47b2ef6848c99751b82baaf0c3b
Vlad Montchik <vlad.montchik@gmail.com>
2022-12-13 19:57:12 -0700

    implement part one

---
+++ DayOne/CalorieCounter.cs
@@ -0,0 +1,51 @@
+﻿namespace DayOne;
+
+public class CalorieCounter
+{
+    private readonly List<List<uint>> _caloriesForAllElves;
+
+    public CalorieCounter()
+    {
+        _caloriesForAllElves = new List<List<uint>>();
+    }
+
+    public void ReadCaloriesFromFile(string inputFilePath)
+    {
+        var lines = File
+            .ReadAllLines(inputFilePath)
+            .ToList();
+
+        _caloriesForAllElves.Clear();
+
+        PopulateCaloriesPerElf(lines);
+    }
+
+    private void PopulateCaloriesPerElf(List<string> lines)
+    {
+        var accumulator = new List<uint>();
+        foreach (var line in lines)
+        {
+            if (line.Equals(string.Empty))
+            {
+                _caloriesForAllElves.Add(accumulator);
+                accumulator = new List<uint>();
+            }
+            else
+            {
+                var calories = Convert.ToUInt32(line);
+                accumulator.Add(calories);
+            }
+        }
+    }
+
+    public uint FindElfWithMostFood()
+    {
+        if (_caloriesForAllElves.Count == 0)
+            throw new Exception("Unable to find max due to empty state. Read calories from file first!");
+
+        return _caloriesForAllElves
+            .Select(caloriesPerElf => caloriesPerElf
+                .Aggregate<uint, uint>(0, (current, calories) => current + calories))
+            .Max();
+    }
+}
---
+++ DayOne/Program.cs
@@ -0,0 +1,8 @@
+﻿using DayOne;
+
+// Part One
+
+var calorieCounter = new CalorieCounter();
+calorieCounter.ReadCaloriesFromFile(@"full.txt");
+var caloriesOfElfWithMostFood = calorieCounter.FindElfWithMostFood();
+Console.WriteLine(caloriesOfElfWithMostFood);
