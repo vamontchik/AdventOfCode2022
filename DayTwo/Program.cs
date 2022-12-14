c762d869296f7443cd73bc234c3687ffd2d59b4d
Vlad Montchik <vlad.montchik@gmail.com>
2022-12-14 15:53:18 -0700

    commit some missing files from day two part one

---
+++ DayTwo/Program.cs
@@ -0,0 +1,8 @@
+﻿using DayTwo;
+
+// Part One
+
+var gameHandler = new GameHandler();
+gameHandler.ReadStrategyGuide("full.txt");
+var score = gameHandler.CalculatePlayerScore();
+Console.WriteLine(score);
