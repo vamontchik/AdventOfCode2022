0a99041fd9132ac1b59a1fb00618d15cfe3de184
Vlad Montchik <vlad.montchik@gmail.com>
2022-12-17 12:41:22 -0700

    day six part one

---
+++ DaySix/Program.cs
@@ -0,0 +1,6 @@
+﻿using DaySix;
+
+var signalReader = new SignalReader();
+signalReader.ReadSignal("full.txt");
+var markerLocation = signalReader.FindMarker();
+Console.WriteLine(markerLocation);
---
+++ DaySix/SignalReader.cs
@@ -0,0 +1,41 @@
+﻿namespace DaySix;
+
+public sealed class SignalReader
+{
+    private string _signal;
+
+    public SignalReader()
+    {
+        _signal = string.Empty;
+    }
+
+    public void ReadSignal(string inputFilePath)
+    {
+        _signal = File.ReadAllText(inputFilePath);
+    }
+
+    public int FindMarker()
+    {
+        for (var i = 0; i < _signal.Length; ++i)
+        {
+            if (MarkerFound(i))
+            {
+                return i + 1; // (+1) since answer is 1-based
+            }
+        }
+
+        throw new Exception("No signal found!");
+    }
+
+    private bool MarkerFound(int i)
+    {
+        if (i < 3)
+        {
+            return false;
+        }
+
+        var charactersToCompare = new List<char> { _signal[i], _signal[i - 1], _signal[i - 2], _signal[i - 3] };
+        var hashSet = new HashSet<char>(charactersToCompare);
+        return hashSet.Count == 4;
+    }
+}
